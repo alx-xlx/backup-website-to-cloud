a237238b78c22d191b0e23e52f281067034cac8d
r4nchy <42188371+alx-xlx@users.noreply.github.com>
2020-10-25 12:40:29 +0530

    update

--- update_backup.sh
+++ update_backup.sh
@@ -1,11 +1,3 @@
-# backup-flarum
- This is a script to push a backup of your Flarum Instance to Cloud Storage using rclone
-
-BUT you can use this even for Wordpress and all other forums
-
-[Click Here] to see the list of Cloud Storages we can upload our Backups to
-
-
 # #!/usr/bin/env bash
 NAMEDATE=`date +%F_%H-%M_%s`_`whoami` && echo $NAMEDATE
 mkdir ~/flarum_backup/$NAMEDATE -m 0755 && echo "Directory Created"
@@ -15,24 +7,3 @@ chmod -R 0644 ~/flarum_backup/$NAMEDATE/* && echo "Directory Permission Restored
 /home/<HOSTINGUSERNAME>/flarum_backup/rclone copy ~/flarum_backup/$NAMEDATE "mycloud:Flarum Backup/$NAMEDATE"
 cd ~/flarum_backup; find . -type d -mtime +2 -exec rm -rf {} \; 2>&1 && echo "Directory older than 2 days Deleted !!"
 exit 0 
-
-## Auto Backup
-
-If you have "Cron Jobs" service in your c-panel then 
-
-
-## Backup Manually
-
-You can simply open SSH and Run
-
-```sh
-bash update_backup.sh
-```
-
-
-
-## Cons
-
-- once While using google drive in rclone, new "Flarum Backup" folders were being created each time a new upload was made (however I couldn't reproduce it later)
-
-- For few cloud storages, your email and password may float around in the .config/rclone/rclone.conf
